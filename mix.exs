cb823df03657489677a9e9642994153af71da5db
mccraigmccraig <mccraigmccraig@gmail.com>
2026-01-17 18:09:44 +0000

    update skuld dep

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule TodosMcp.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:skuld, "~> 0.1.12"},
+      {:skuld, "~> 0.1.13"},
       {:phoenix, "~> 1.8.3"},
       {:phoenix_ecto, "~> 4.5"},
       {:ecto_sql, "~> 3.13"},
